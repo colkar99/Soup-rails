50f799c53f8d5fd26580a6a4a741a0f0f642b588
colkar99 <colkar99@gmail.com>
2017-10-03 18:06:05 +0530

    first commit

---
+++ app/controllers/soups_controller.rb
@@ -0,0 +1,19 @@
+class SoupsController < ApplicationController
+	before_action :fetch_soup, only: [:show, :edit, :update, :destroy]
+	def show
+		respond_to do |format|
+			format.html
+			format.json {render json: @soup}
+		end
+	end
+	def index
+		@soups = Soup.all
+		respond_to do |format|
+			format.json {render json: @soups}
+		end
+	end
+	private
+	def fetch_soup
+		@soup = Soup.find(params[:id])
+	end
+end
